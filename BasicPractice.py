b951443dae8072ce7416ee2782620041b2107b3f
Junggyu <cjg17@naver.com>
2023-01-02 22:54:18 +0900

    Update Python Basic File[py]

--- BasicPractice.py
+++ BasicPractice.py
@@ -1,6 +1,40 @@
+print("--------- Basic Practice ----------")
+
+# 문자자료형
 print('Hello World')
 print("안녕하세요.")
+
+# 숫자자료형
 print(1)
 print(3.15)
+
+# Boolean자료형
 print(True)
 print(False)
+'''꼭 앞글자는 대문자로 되어야한다'''
+
+print("-----------------------------------")
+
+# 변수
+variableTxt = 'Text 변수'
+variableNum = 3560
+
+print(variableTxt)
+print(variableNum)
+
+# 변수의 규칙
+var = '문자'
+_var = '밑줄 문자'
+var_1 = '문자 밑줄 숫자 혼합'
+'''변수명에 공백 or 특수문자 사용불가'''
+
+Var = '앞 대문자 변수'
+VAR = '전체 대문자 변수'
+
+print(var)
+print(_var)
+print(var_1)
+print(Var)
+print(VAR)
+
+print("-----------------------------------")
